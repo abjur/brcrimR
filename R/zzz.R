48234cab507074e965a912c15d0d07396243c213
Fernando Corrêa <fptcorrea@gmail.com>
2017-08-29 19:11:59 -0300

    move get_viewstate to zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,16 @@
-#'@export
+#' @export
 `%>%` <- magrittr::`%>%`
+
+get_viewstate <- function(r){
+  view_state <- r %>%
+    xml2::read_html() %>%
+    rvest::html_nodes("input[name='__VIEWSTATE']") %>%
+    rvest::html_attr("value")
+
+  event_validation <- r %>%
+    xml2::read_html() %>%
+    rvest::html_nodes("input[name='__EVENTVALIDATION']") %>%
+    rvest::html_attr("value")
+
+  return(list(vs = view_state, ev = event_validation))
+}
