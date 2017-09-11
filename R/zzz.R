41b5a82bb63ac0dad541a5159579497b769c0591
Fernando Corrêa <fptcorrea@gmail.com>
2017-09-11 18:39:59 -0300

    add number parser

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,9 @@
 #' @export
 `%>%` <- magrittr::`%>%`
 
-get_viewstate <- function(r){
-  view_state <- r %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("input[name='__VIEWSTATE']") %>%
-    rvest::html_attr("value")
-
-  event_validation <- r %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("input[name='__EVENTVALIDATION']") %>%
-    rvest::html_attr("value")
-
-  return(list(vs = view_state, ev = event_validation))
+parse_number_br <- function(char_numbers){
+  char_numbers %>%
+    stringr::str_replace_all("\\.", "") %>%
+    stringr::str_replace_all(",", "\\.") %>%
+    as.numeric()
 }
